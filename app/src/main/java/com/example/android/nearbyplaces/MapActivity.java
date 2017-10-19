93a0722993ef3374986e0be3325b074785d1c0d4
kamathshashwath1991 <kamathshashwath1991@gmail.com>
2017-10-18 20:01:26 -0400

    Initial commit

---
+++ app/src/main/java/com/example/android/nearbyplaces/MapActivity.java
@@ -0,0 +1,19 @@
+package com.example.android.nearbyplaces;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by shash on 10/18/2017.
+ */
+
+public class MapActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_map);
+    }
+}
