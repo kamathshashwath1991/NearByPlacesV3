2df1e52ac6f48d1ddb47bb78c6a852fdf9ae3fe9
kamathshashwath1991 <kamathshashwath1991@gmail.com>
2017-10-18 23:04:34 -0400

    Maps integrated

--- app/src/main/java/com/example/android/nearbyplaces/MapActivity.java
+++ app/src/main/java/com/example/android/nearbyplaces/MapActivity.java
@@ -1,19 +1,103 @@
 package com.example.android.nearbyplaces;
 
+import android.*;
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 
 /**
  * Created by shash on 10/18/2017.
  */
 
-public class MapActivity extends AppCompatActivity {
+public class MapActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private static final String TAG = "MapActivity";
+
+    private static final String FINE_LOCATION= Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COARSE_LOCATION= Manifest.permission.ACCESS_COARSE_LOCATION;
+
+    private Boolean mLocationPermissionGranted= false;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1234;
+
+    private GoogleMap mMap;
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_map);
+        getLocationPermission();
     }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Log.d(TAG, "onMapReady: Map is ready here");
+        Toast.makeText(this, "Map is ready", Toast.LENGTH_SHORT).show();
+        mMap= googleMap;
+    }
+
+    private void initMap() {
+        Log.d(TAG, "initMap: initializing map");
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(MapActivity.this);
+    }
+
+    /*
+    Asking user to grant permission
+     */
+    private void getLocationPermission(){
+        Log.d(TAG, "getLocationPermission: gettting location permission");
+        
+        String[] permissions ={Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION};
+
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),COARSE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionGranted= true;
+            }
+            else {
+                ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }
+    }
+
+    /*
+    if permit has been given or not given, decide the behaviour.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionGranted= false; // why did he do this false
+
+        switch(requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:{
+                if (grantResults.length>0 ){
+                    for (int i=0; i<grantResults.length;i++){
+                        mLocationPermissionGranted = false;
+                        Log.d(TAG, "onRequestPermissionsResult: permission failed");
+                        return;
+                    }
+                    Log.d(TAG, "onRequestPermissionsResult: permission granted");
+                    mLocationPermissionGranted= true;
+                    //initialize map
+                    initMap();
+                }
+
+            }
+        }
+
+    }
+
 }
